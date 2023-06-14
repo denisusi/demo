d9ba0b5cc43f3ce41b5ceaf8b3c0050c806912bf
deni <deni4arkata@gmail.com>
2023-06-14 15:40:16 +0300

    add demo project

---
+++ Proba1_david holding/Proba1_david holding/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Proba1_david_holding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
